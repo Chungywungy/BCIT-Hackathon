049ffd7b224bcb670ee75d5d67dad793c4248b74
Oliver Wright <bwright52@my.bcit.ca>
2026-03-14 08:40:37 -0700

    Minor refactor to properly implement helpers from identify_operation.py

--- identify_assignment.py
+++ identify_assignment.py
@@ -1,31 +1,32 @@
 import identify_operation
 
 
-def identify_assignment(split_string: list):
-    for line in split_string:
-        if "=" in line and "==" not in line:
-            print_assignment(line)
+def identify_assignment(split_string: str):
+    if "=" in split_string and "==" not in split_string:
+        return True
+    return None
+
 
 def print_assignment(split_string_line: str):
     string_parts = split_string_line.split("=")
-    assignee = string_parts[0]
+    assignee = string_parts[0].strip()
     string_parts.pop(0)
     index = 0
     assigner = ""
     while index < len(string_parts):
-        assigner = identify_operation.identify_operations(string_parts[index])
+        assigner = identify_operation.identify_operations(string_parts[index].strip())
         index += 1
 
     print(f"Sets {assignee} equal to {assigner}")
 
 
 def main():
-    my_string = "def my_function(x, y): \n x = y"
-    my_split_string = my_string.split("\n")
-    identify_assignment(my_split_string)
-    my_string = "def my_function(x, y): \n x = y + z"
-    my_split_string = my_string.split("\n")
-    identify_assignment(my_split_string)
+    my_string = "x = y"
+    if identify_assignment(my_string):
+        print_assignment(my_string)
+    my_string = "x = y + z"
+    if identify_assignment(my_string):
+        print_assignment(my_string)
 
 
 if __name__ == "__main__":
