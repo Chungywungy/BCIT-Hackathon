6abc305445a1466c05b68ba0477b57785fa3f544
James Garcia <jgarcia97@my.bcit.ca>
2026-03-14 20:09:22 -0700

    Fix while loop logic

--- iterables.py
+++ iterables.py
@@ -4,27 +4,10 @@ from identify_functions import *
 
 def while_loop(split_string):
     if "while" in split_string:
-        storage = []
-        cache = []
-        colon_checker = False
-        for letter in split_string:
-            if letter != ":":
-                storage.append(letter)
-            else:
-                break
-
-        storage = "".join(storage)
-        for letter in split_string:
-            if colon_checker:
-                cache.append(letter)
-            if letter == ":":
-                colon_checker = True
+        storage = split_string.split(":")[0].split("while")[1].strip()
 
 
-        cache = "".join(cache)
-
-
-        return f'{identify_comparison_operators(storage)} is true, it repeatedly executes'f' {identify_operations(cache)}.'
+        return f'While {replaces_function_calls(identify_comparison_operators(storage))} is true, it repeatedly executes'
 
 
 def for_loop(split_string):
@@ -42,23 +25,26 @@ def conditionals(split_string: list):
     container = []
 
     for line in split_string:
-        contains_cond = False
+        non_applicable = False
         for cond in {"elif", "else", "if"}:
             if cond in line:
                 argument = line[len(cond):].strip().split(":")[0]
                 container.append(f"{lambdamentality[cond](argument)}" )
-                contains_cond = True
+                non_applicable = True
                 break
 
             while_output = while_loop(line)
             if while_output:
                 container.append(while_output)
+                non_applicable = True
                 break
             for_output = for_loop(line)
             if for_output:
                 container.append(for_output)
+                non_applicable = True
                 break
-        if not contains_cond:
+
+        if not non_applicable:
             value = replaces_function_calls(identify_comparison_operators(line))
             container.append(value.strip()+"\n")
 
@@ -70,7 +56,8 @@ def main():
     code_block_while_loop = "while alakazam > 1: \n  abra + cadabra"
     code_block_for_loop = "for abra in cadabra: \n  do_something()"
     code_block_conditionals = ["if 1 > int(2):", "do_anything()", "elif 1 < range(2):", "please_work()", "else:", "1 + 2"]
-    return conditionals(code_block_conditionals)
+    sample = ["while True:"]
+    return conditionals(sample)
 
 if __name__ == "__main__":
     print(main())
